2c4c7b382fea9a1fa79a84e11caf19bd77712add
YueLineMe <949861884@qq.com>
2018-11-26 09:38:24 +0800

    2018-11-26

---
+++ pages/newsInfo/newsInfo.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<view id="newInfo" v-html="body"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				body:[]
+			};
+		},
+		methods:{
+			createLink(link){
+				/* var link=document.createElement('link');
+				link.rel="stylesheet";
+				link.type="text/css";
+				link.href=link;
+				document.getElementsByTagName("head")[0].appendChild(link); */
+			}
+		},
+		onLoad:function(e){
+			uni.request({
+				url: 'http://news-at.zhihu.com/api/4/news/'+e.id,
+				success: res => {
+					/* this.createLink(res.data.css); */
+					this.body=res.data.body;
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		}
+	}
+</script>
+
+<style>
+
+</style>
